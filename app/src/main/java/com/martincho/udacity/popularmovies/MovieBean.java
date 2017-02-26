6e8f6e4525595060e17b444cebd44b8dbcbce13d
martincho <martinjuarez77@gmail.com>
2017-02-26 09:18:04 +0100

    MovieBean Serialized to Parcelable

--- app/src/main/java/com/martincho/udacity/popularmovies/MovieBean.java
+++ app/src/main/java/com/martincho/udacity/popularmovies/MovieBean.java
@@ -1,18 +1,31 @@
 package com.martincho.udacity.popularmovies;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by martincho on 28/1/17.
  */
 
-public class MovieBean implements Serializable {
+public class MovieBean  implements Parcelable {
 
     private String title;
     private String releaseDate;
     private String moviePoster;
     private String voteAverage;
     private String plotSynopsis;
+
+    public MovieBean(){
+
+    }
+
+    public MovieBean(Parcel _movieBean) {
+        this.title = _movieBean.readString();
+        this.releaseDate = _movieBean.readString();
+        this.moviePoster = _movieBean.readString();
+        this.voteAverage = _movieBean.readString();
+        this.plotSynopsis = _movieBean.readString();
+    }
 
     public String getTitle() {
         return title;
@@ -53,5 +66,31 @@ public class MovieBean implements Serializable {
     public void setPlotSynopsis(String plotSynopsis) {
         this.plotSynopsis = plotSynopsis;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(releaseDate);
+        parcel.writeString(moviePoster);
+        parcel.writeString(voteAverage);
+        parcel.writeString(plotSynopsis);
+
+    }
+
+
+    public static final Parcelable.Creator<MovieBean> CREATOR = new Parcelable.Creator<MovieBean>() {
+        public MovieBean createFromParcel(Parcel movieBean) {
+            return new MovieBean(movieBean);
+        }
+
+        public MovieBean[] newArray(int size) {
+            return new MovieBean[size];
+        }
+    };
 
 }
